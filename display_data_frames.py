5a125f7ef4772678f59d0c8b6f1aba8a8a684efa
Nick-Kevin <nicblou23@gmail.com>
2025-10-24 18:41:17 +0300

    update : display_data_frames - Filter rows where Age is greater than 30

--- display_data_frames.py
+++ display_data_frames.py
@@ -4,18 +4,24 @@
 
 import pandas as pd
 
-dataFrames = {
+# data framres
+
+data = {
     "Name": ["John", "Cathy", "Ross", "Jack"], 
     "Age": [28, 47, 36, 51],
     "City": ["Los Angeles", "Chicago", "Miami", "New York"]
 }
 
-df = pd.DataFrame(dataFrames)
-
+df = pd.DataFrame(data)
 print("The date frames")
 print(df)
 
 age_series = df["Age"]
-
 print("\n The Age column is extracted:")
 print(age_series)
+
+# Data Manipulation Tasks
+
+adult = df[age_series>30]
+print("\n Filter rows where Age is greater than 30:")
+print(adult)
