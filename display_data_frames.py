bf0a0c650280c307d922046b480fc839ee2e8c56
Nick-Kevin <nicblou23@gmail.com>
2025-10-24 19:11:37 +0300

    update : display_data_frames - slicing

--- display_data_frames.py
+++ display_data_frames.py
@@ -22,6 +22,17 @@ print(age_series)
 
 # Data Manipulation Tasks
 
-adult = df[age_series>30]
+# filter
+adult = df.query("Age>30")
 print("\n Filter rows where Age is greater than 30:")
 print(adult)
+
+#slicing
+last_two_rows = df.iloc[2:4, 0:2]
+print("\n Select rows 2 to 3 and columns 0 to 1")
+print(last_two_rows)
+
+df.set_index("Name", inplace=True)
+two_first_rows = df.loc["John":"Cathy", "Age":"City"]
+print("\n The two first rows")
+print(two_first_rows)
